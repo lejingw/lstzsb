56b457bb6d2c6d8b07d8ff2351c38c868b494ce4
王乐静 <wanglj@wanglaifeng.local>
2013-11-12 17:23:00 +0800

    milestone

--- tzsb/src/test/resources/lstzsb_db_2013-11-12.sql
+++ tzsb/src/test/resources/lstzsb_db_2013-11-12.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.6.14)
 # Database: lstzsb_db
-# Generation Time: 2013-11-11 11:44:08 +0000
+# Generation Time: 2013-11-12 09:20:33 +0000
 # ************************************************************
 
 
@@ -98,9 +98,14 @@ LOCK TABLES `jat_move_head` WRITE;
 
 INSERT INTO `jat_move_head` (`headid`, `billno`, `dodate`, `in_org_id`, `out_org_id`, `create_id`, `create_date`, `update_id`, `update_date`, `status`, `sum_count`, `sum_weight`, `memo`)
 VALUES
-	(11,'111111N','2013-11-11',NULL,NULL,1,'2013-11-11 10:49:16',1,'2013-11-11 10:49:16',1,1,10.1,''),
-	(13,NULL,'2013-11-11',NULL,NULL,1,'2013-11-11 11:42:00',1,'2013-11-11 11:42:00',1,1,10.1,''),
-	(14,'001','2013-11-11',4,1,1,'2013-11-11 12:55:06',1,'2013-11-11 13:07:48',2,1,5,'aaa11');
+	(21,'TB201311120001','2013-11-12',6,1,1,'2013-11-12 12:40:39',1,'2013-11-12 12:40:39',1,2,15.1,''),
+	(22,'TB201311120002','2013-11-12',4,1,1,'2013-11-12 12:41:06',1,'2013-11-12 15:18:37',1,2,15.1,''),
+	(23,'TB201311120003','2013-11-12',4,1,1,'2013-11-12 15:41:14',1,'2013-11-12 15:46:04',2,1,10.1,''),
+	(24,'TB201311120004','2013-11-12',1,1,1,'2013-11-12 15:45:42',1,'2013-11-12 15:45:42',1,2,15.1,''),
+	(25,'TB201311120005','2013-11-12',4,1,1,'2013-11-12 16:09:17',1,'2013-11-12 16:09:17',1,1,10.1,''),
+	(26,'TB201311120006','2013-11-12',3,1,1,'2013-11-12 16:11:31',1,'2013-11-12 16:11:31',1,1,10.1,''),
+	(27,'TB201311120007','2013-11-12',3,1,1,'2013-11-12 16:14:31',1,'2013-11-12 16:14:31',1,1,10.1,''),
+	(28,'TB201311120008','2013-11-12',3,1,1,'2013-11-12 16:26:46',1,'2013-11-12 16:26:46',1,1,10.1,'');
 
 /*!40000 ALTER TABLE `jat_move_head` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -131,9 +136,17 @@ LOCK TABLES `jat_move_line` WRITE;
 
 INSERT INTO `jat_move_line` (`lineid`, `headid`, `orna_code`, `orna_dsc`, `weight`, `create_id`, `create_date`, `update_id`, `update_date`, `status`, `memo`)
 VALUES
-	(22,11,'0001','Mac一体机',10.1,NULL,NULL,NULL,NULL,NULL,NULL),
-	(24,13,'0001','Mac一体机',10.1,1,'2013-11-11 11:42:00',1,'2013-11-11 11:42:00',1,NULL),
-	(27,14,'0003','Mac打印机',5,1,'2013-11-11 13:00:59',1,'2013-11-11 13:00:59',1,NULL);
+	(34,21,'0003','Mac打印机',5,1,'2013-11-12 12:40:39',1,'2013-11-12 12:40:39',1,NULL),
+	(35,21,'0001','Mac一体机',10.1,1,'2013-11-12 12:40:39',1,'2013-11-12 12:40:39',1,NULL),
+	(36,22,'0001','Mac一体机',10.1,1,'2013-11-12 12:41:06',1,'2013-11-12 12:41:06',1,NULL),
+	(37,22,'0003','Mac打印机',5,1,'2013-11-12 15:18:37',1,'2013-11-12 15:18:37',1,NULL),
+	(38,23,'0001','Mac一体机',10.1,1,'2013-11-12 15:41:14',1,'2013-11-12 15:41:14',1,NULL),
+	(39,24,'0003','Mac打印机',5,1,'2013-11-12 15:45:42',1,'2013-11-12 15:45:42',1,NULL),
+	(40,24,'0001','Mac一体机',10.1,1,'2013-11-12 15:45:42',1,'2013-11-12 15:45:42',1,NULL),
+	(41,25,'0001','Mac一体机',10.1,1,'2013-11-12 16:09:17',1,'2013-11-12 16:09:17',1,NULL),
+	(42,26,'0001','Mac一体机',10.1,1,'2013-11-12 16:11:31',1,'2013-11-12 16:11:31',1,NULL),
+	(43,27,'0001','Mac一体机',10.1,1,'2013-11-12 16:14:31',1,'2013-11-12 16:14:31',1,NULL),
+	(44,28,'0001','Mac一体机',10.1,1,'2013-11-12 16:26:46',1,'2013-11-12 16:26:46',1,NULL);
 
 /*!40000 ALTER TABLE `jat_move_line` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -191,7 +204,7 @@ LOCK TABLES `jat_sys_dict_entry` WRITE;
 
 INSERT INTO `jat_sys_dict_entry` (`entry_code`, `entry_name`, `entry_type`, `memo`, `create_date`, `create_id`, `update_date`, `update_id`, `status`)
 VALUES
-	('hqorgs','总部组织','1',NULL,'2012-01-01 00:00:00',1,'2012-01-01 00:00:00',1,1),
+	('billcode','单据编码','1',NULL,'2012-01-01 00:00:00',1,'2012-01-01 00:00:00',1,1),
 	('status','单据状态','1',NULL,'2012-01-01 00:00:00',1,'2012-01-01 00:00:00',1,1);
 
 /*!40000 ALTER TABLE `jat_sys_dict_entry` ENABLE KEYS */;
@@ -223,9 +236,61 @@ VALUES
 	('0','status','停用',0,1,1,'2012-01-01 00:00:00','2012-01-01 00:00:00',1),
 	('1','status','保存',1,1,1,'2012-01-01 00:00:00','2012-01-01 00:00:00',1),
 	('2','status','记账',2,1,1,'2012-01-01 00:00:00','2012-01-01 00:00:00',1),
-	('1','hqorgs','中国配送中心',1,1,1,'2012-01-01 00:00:00','2012-01-01 00:00:00',1);
+	('TB','billcode','调拨单',1,1,1,'2012-01-01 00:00:00','2012-01-01 00:00:00',1);
 
 /*!40000 ALTER TABLE `jat_sys_dict_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table jat_sys_index
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `jat_sys_index`;
+
+CREATE TABLE `jat_sys_index` (
+  `biz_type` varchar(20) NOT NULL DEFAULT '',
+  `biz_code` varchar(20) NOT NULL DEFAULT '',
+  `biz_date` varchar(20) DEFAULT NULL,
+  `biz_index` int(11) DEFAULT NULL,
+  PRIMARY KEY (`biz_type`,`biz_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `jat_sys_index` WRITE;
+/*!40000 ALTER TABLE `jat_sys_index` DISABLE KEYS */;
+
+INSERT INTO `jat_sys_index` (`biz_type`, `biz_code`, `biz_date`, `biz_index`)
+VALUES
+	('billNo','TB','20131112',8);
+
+/*!40000 ALTER TABLE `jat_sys_index` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table jat_sys_parameter
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `jat_sys_parameter`;
+
+CREATE TABLE `jat_sys_parameter` (
+  `param_name` varchar(20) NOT NULL DEFAULT '',
+  `param_value` varchar(1024) DEFAULT NULL,
+  `memo` varchar(1024) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `create_id` int(11) DEFAULT NULL,
+  `create_date` varchar(20) DEFAULT NULL,
+  `update_id` int(11) DEFAULT NULL,
+  `update_date` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`param_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `jat_sys_parameter` WRITE;
+/*!40000 ALTER TABLE `jat_sys_parameter` DISABLE KEYS */;
+
+INSERT INTO `jat_sys_parameter` (`param_name`, `param_value`, `memo`, `status`, `create_id`, `create_date`, `update_id`, `update_date`)
+VALUES
+	('picBasicPath','/upload','图片上传路径',1,1,'2012-01-01 00:00:00',1,'2012-01-01 00:00:00');
+
+/*!40000 ALTER TABLE `jat_sys_parameter` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
