1a9dbcf8f86157bb00117912bc7ae67106bb7976
lejing wang <lejingw@163.com>
2013-11-20 10:49:40 +0800

    1

--- tzsb/src/main/java/com/lejingw/apps/ws4/Hello4WsService.java
+++ tzsb/src/main/java/com/lejingw/apps/ws4/Hello4WsService.java
@@ -1,14 +1,9 @@
 package com.lejingw.apps.ws4;
 
-import javax.jws.WebParam;
 import javax.jws.WebService;
-import javax.jws.soap.SOAPBinding;
-import javax.xml.ws.soap.MTOM;
 
-@WebService(name="Hello4WsService") 
-@SOAPBinding(style = SOAPBinding.Style.RPC) 
-@MTOM
+
+@WebService(targetNamespace="http://ws4.apps.lejingw.com")
 public interface Hello4WsService {
-	public void uploadCustomerByName(
-			@WebParam(name = "customer") Customer customer); 
+	public void uploadCustomerByName(Customer customer);
 }
--- tzsb/src/main/java/com/lejingw/apps/ws4/Hello4WsServiceImpl.java
+++ tzsb/src/main/java/com/lejingw/apps/ws4/Hello4WsServiceImpl.java
@@ -4,22 +4,29 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.xml.ws.soap.MTOM;
 
-@WebService(serviceName = "Hello4WsService",
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+@MTOM
+@WebService(name = "Hello4WsService",
+			serviceName = "Hello4WsService",
 			portName = "Hello4WsServicePort",
-			targetNamespace = "http://ws4.apps.lejingw.com/",
+			targetNamespace = "http://ws4.apps.lejingw.com",
 			endpointInterface = "com.lejingw.apps.ws4.Hello4WsService",
-			wsdlLocation="/WEB-INF/wsdl4/Hello4WsService.wsdl")
+			wsdlLocation="/WEB-INF/wsdl4/Hello4WsService.wsdl"
+			)
 public class Hello4WsServiceImpl implements Hello4WsService {
 
-	@Override
-	public void uploadCustomerByName(Customer customer) {
+	@WebMethod
+	public void uploadCustomerByName(@WebParam(name = "customer") Customer customer) {
 		InputStream is = null;
 		OutputStream os = null;
 		try {
-			System.out.println("customer====" + customer.getId() + "  "
-					+ customer.getName());
+			System.out.println("customer====" + customer.getId() + "  " + customer.getName());
 			System.out.println("generate jpg......");
 			is = customer.getMyPhoto().getInputStream();
 			os = new FileOutputStream("/Users/wanglj/Desktop/abc.jpg");
