ecd8ca8cb7b86e6beb85ae555200d11cf989787f
lejing wang <lejingw@163.com>
2013-12-23 19:59:44 +0800

    1

--- tzsb/src/main/webapp/script/ueditor/ueditor.config.js
+++ tzsb/src/main/webapp/script/ueditor/ueditor.config.js
@@ -20,6 +20,7 @@
      * window.UEDITOR_HOME_URL = "/xxxx/xxxx/";
      */
     var URL = window.UEDITOR_HOME_URL || getUEBasePath();
+
     /**
      * 配置项主体。注意，此处所有涉及到路径的配置别遗漏URL变量。
      */
@@ -29,40 +30,35 @@
         UEDITOR_HOME_URL : URL
 
         //图片上传配置区
-        //,imageUrl:URL+"jsp/imageUp.jsp"             //图片上传提交地址
-        ,imageUrl:ctxPath + "/uploadServlet?type=1"
-        //,imagePath:URL + "jsp/"                     //图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
-        ,imagePath:ctxPath
+        ,imageUrl:URL+"jsp/imageUp.jsp"             //图片上传提交地址
+        ,imagePath:URL + "jsp/"                     //图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
         //,imageFieldName:"upfile"                   //图片数据的key,若此处修改，需要在后台对应文件修改对应参数
         //,compressSide:0                            //等比压缩的基准，确定maxImageSideLength参数的参照对象。0为按照最长边，1为按照宽度，2为按照高度
         //,maxImageSideLength:900                    //上传图片最大允许的边长，超过会自动等比缩放,不缩放就设置一个比较大的值，更多设置在image.html中
-        ,savePath: ['upload']                        //图片保存在服务器端的目录， 默认为空， 此时在上传图片时会向服务器请求保存图片的目录列表，
+        //,savePath: [ 'upload1', 'upload2', 'upload3' ]      //图片保存在服务器端的目录， 默认为空， 此时在上传图片时会向服务器请求保存图片的目录列表，
                                                             // 如果用户不希望发送请求， 则可以在这里设置与服务器端能够对应上的目录名称列表
                                                             //比如： savePath: [ 'upload1', 'upload2' ]
 
         //涂鸦图片配置区
         ,scrawlUrl:URL+"jsp/scrawlUp.jsp"           //涂鸦上传地址
-        ,scrawlPath:URL+"jsp/"                      //图片修正地址，同imagePath
+        ,scrawlPath:URL+"jsp/"                            //图片修正地址，同imagePath
 
         //附件上传配置区
-        //,fileUrl:URL+"jsp/fileUp.jsp"              //附件上传提交地址
-    	,fileUrl:ctxPath + "/uploadServlet?type=2"
-        ,filePath:ctxPath                            //附件修正地址，同imagePath
+        ,fileUrl:URL+"jsp/fileUp.jsp"               //附件上传提交地址
+        ,filePath:URL + "jsp/"                   //附件修正地址，同imagePath
         //,fileFieldName:"upfile"                    //附件提交的表单名，若此处修改，需要在后台对应文件修改对应参数
 
         //远程抓取配置区
         //,catchRemoteImageEnable:true               //是否开启远程图片抓取,默认开启
-        ,catcherUrl:URL +"jsp/getRemoteImage.jsp"    //处理远程图片抓取的地址
-        ,catcherPath:URL + "jsp/"                    //图片修正地址，同imagePath
+        ,catcherUrl:URL +"jsp/getRemoteImage.jsp"   //处理远程图片抓取的地址
+        ,catcherPath:URL + "jsp/"                  //图片修正地址，同imagePath
         //,catchFieldName:"upfile"                   //提交到后台远程图片uri合集，若此处修改，需要在后台对应文件修改对应参数
-        //,separater:'ue_separate_ue'                //提交至后台的远程图片地址字符串分隔符
+        //,separater:'ue_separate_ue'               //提交至后台的远程图片地址字符串分隔符
         //,localDomain:[]                            //本地顶级域名，当开启远程图片抓取时，除此之外的所有其它域名下的图片都将被抓取到本地,默认不抓取127.0.0.1和localhost
 
         //图片在线管理配置区
-        //,imageManagerUrl:URL + "jsp/imageManager.jsp"       //图片在线管理的处理地址
-        ,imageManagerUrl:ctxPath + "/uploadServlet?type=3"
-        //,imageManagerPath:URL + "jsp/"                      //图片修正地址，同imagePath
-        ,imageManagerPath: location.protocol + "//"+location.hostname+":"+location.port + ctxPath
+        ,imageManagerUrl:URL + "jsp/imageManager.jsp"       //图片在线管理的处理地址
+        ,imageManagerPath:URL + "jsp/"                                    //图片修正地址，同imagePath
 
         //屏幕截图配置区
         ,snapscreenHost: location.hostname                                 //屏幕截图的server端文件所在的网站地址或者ip，请不要加http://
@@ -88,10 +84,10 @@
                 'directionalityltr', 'directionalityrtl', 'indent', '|',
                 'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|', 'touppercase', 'tolowercase', '|',
                 'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
-                'insertimage', 'emotion', /*'scrawl', 'insertvideo', 'music', */'attachment', 'map', /*'gmap',*/'insertframe',/*'insertcode','webapp',*/'pagebreak', 'template', 'background', '|',
-                'horizontal', 'date', 'time', 'spechars', /*'snapscreen',*/ 'wordimage', '|',
+                'insertimage', 'emotion', 'scrawl', 'insertvideo', 'music', 'attachment', 'map', 'gmap', 'insertframe','insertcode', 'webapp', 'pagebreak', 'template', 'background', '|',
+                'horizontal', 'date', 'time', 'spechars', 'snapscreen', 'wordimage', '|',
                 'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', 'charts', '|',
-                'print', 'preview', 'searchreplace', /*'help',*/'drafts']
+                'print', 'preview', 'searchreplace', 'help', 'drafts']
         ]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
 //        ,labelMap:{
@@ -125,7 +121,7 @@
         //常用配置项目
         //,isShow : true    //默认显示编辑器
 
-        //,initialContent:'欢迎使用!'    //初始化编辑器的内容,也可以通过textarea/script给值，看官网例子
+        //,initialContent:'欢迎使用ueditor!'    //初始化编辑器的内容,也可以通过textarea/script给值，看官网例子
 
         //,initialFrameWidth:1000  //初始化编辑器宽度,默认1000
         //,initialFrameHeight:320  //初始化编辑器高度,默认320
