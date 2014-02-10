2d17c5f98870bf1b15ce9189a5d5e28635809904
lejing wang <lejingw@163.com>
2014-02-10 10:52:17 +0800

    1

--- tzsb/src/main/java/com/totyu/common/CommonUtil.java
+++ tzsb/src/main/java/com/totyu/common/CommonUtil.java
@@ -451,4 +451,32 @@ public class CommonUtil {
 			return null;
 		}
 	}
+	/**
+	 * 获取request的IP地址
+	 * @param request
+	 * @return
+	 */
+    public static String getIpAddr(HttpServletRequest request) {
+        if (request == null) {
+            return "unknown";
+        }
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Forwarded-For");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
 }
